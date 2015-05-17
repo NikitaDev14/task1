be0353fa9a79ceb20eb2c7f2f2afdc1c5c0e3afd
Nikita <mykytababych1989@gmail.com>
2015-05-17 11:37:52 +0300

    list+ filters+ details+ order+ orderList+ login+ logout+ signin+ response multiformat+ error handler-

--- Server/api/Views/XmlView.php
+++ Server/api/Views/XmlView.php
@@ -4,11 +4,54 @@
 
 	class XmlView extends BaseView
     {
+		private $writer;
+
+		public function __construct()
+		{
+			$this->writer = new \XMLWriter();
+		}
+
+		private function getXML($data)
+		{
+			foreach($data as $key => $value)
+			{
+				if(is_numeric($key))
+				{
+					$key = 'item' . $key;
+				}
+
+				if(is_array($value))
+				{
+					$this->writer->startElement($key);
+					$this->getXML($value);
+					$this->writer->endElement();
+				}
+				else
+				{
+					$this->writer->writeElement($key, $value);
+				}
+			}
+		}
+		private function xmlEncode($data)
+		{
+			$this->writer->openMemory();
+			$this->writer->startDocument('1.0', 'utf-8');
+			$this->writer->startElement('response');
+
+			if(is_array($data))
+			{
+				$this->getXML($data);
+			}
+
+			$this->writer->endElement();
+
+			return $this->writer->outputMemory();
+		}
         public function response($responseContent)
         {
-            $response = new DOMDocument('1.0', 'utf-8');
+            header(HEADER_XML);
 
-
+	        echo $this->xmlEncode($responseContent);
         }
         public function responseError()
 		{
